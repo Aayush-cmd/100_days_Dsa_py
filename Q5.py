b23d42625a3f0ab71b22262a5ec20df4d4ecfd41
Ayush Kulkarni <ayushkulkarni09@gmail.com>
2024-11-20 23:22:47 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,11 @@
+#Day 5 coding Statement:  Write a program to identify if the number is even or odd
+
+num = float(input("Enter a number: "))
+
+if num %2 == 0 :
+    print("Its an Even number")
+elif num% 2 != 0 :
+    print("Its an Odd number")
+else :
+    print("Invalid")
+
